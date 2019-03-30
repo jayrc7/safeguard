cf1b9cb4224c6d06e8db8d05a0d1075097fb55dd
Jason Cabrera <cabrerajason7@gmail.com>
2019-03-30 05:56:30 -0700

    added server

--- src/components/home.js
+++ src/components/home.js
@@ -2,11 +2,46 @@ import React, { Component } from 'react';
 import { Button } from 'semantic-ui-react';
 
 class Home extends Component{
+    constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      greeting: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ name: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
+      .then(response => response.json())
+      .then(state => this.setState(state));
+  }
+
+
     render(){
         return(
             <div>
                 <h1> Hello </h1>
                 <Button> Button </Button>
+
+            <form onSubmit={this.handleSubmit}>
+            <label htmlFor="name">Enter your name: </label>
+            <input
+              id="name"
+              type="text"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+            <button type="submit">Submit</button>
+          </form>
+          <p>{this.state.greeting}</p>
+                
             </div> 
         );
     }
