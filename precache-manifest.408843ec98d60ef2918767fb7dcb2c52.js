ad6071a26fe2ec7f8cffb9200144349df77894bc
Jason Cabrera <cabrerajason7@gmail.com>
2019-03-30 05:40:54 -0700

    Updates

---
+++ precache-manifest.408843ec98d60ef2918767fb7dcb2c52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/safeguard/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "12546fabf26db12d3b66",
+    "url": "/safeguard/static/js/runtime~main.23e1615c.js"
+  },
+  {
+    "revision": "c1d180b6f7dfb1b21bfc",
+    "url": "/safeguard/static/js/main.af0e2b78.chunk.js"
+  },
+  {
+    "revision": "56e685ef0ed26b905c0f",
+    "url": "/safeguard/static/js/2.079b4311.chunk.js"
+  },
+  {
+    "revision": "c1d180b6f7dfb1b21bfc",
+    "url": "/safeguard/static/css/main.7c34f069.chunk.css"
+  },
+  {
+    "revision": "a4db351f06eeb76a48a27bb025338970",
+    "url": "/safeguard/index.html"
+  }
+];
