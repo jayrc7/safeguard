737de1c3edb977f6956bc414a40d30c1da8342c4
alexmfranklin <amfa2017@mymail.pomona.edu>
2019-03-30 19:50:10 -0700

    added schedule table to patrol page

--- src/components/patrols/Schedule.js
+++ src/components/patrols/Schedule.js
@@ -2,14 +2,191 @@ import React from 'react'
 import ScheduleTable from './ScheduleTable'
 import { Tab } from 'semantic-ui-react'
 
+const patrolDataExample = [
+  {
+    time: '07:00',
+    user: '',
+    route: '',
+    completed: ''
+  },
+  {
+    time: '07:30',
+    user: '',
+    route: '',
+    completed: ''
+  },
+  {
+    time: '08:00',
+    user: '',
+    route: '',
+    completed: ''
+  },
+  {
+    time: '08:30',
+    user: '',
+    route: '',
+    completed: ''
+  },
+  {
+    time: '09:00',
+    user: '',
+    route: '',
+    completed: ''
+  },
+  {
+    time: '09:30',
+    user: '',
+    route: '',
+    completed: ''
+  },
+  {
+    time: '10:00',
+    user: '',
+    route: '',
+    completed: ''
+  },
+  {
+    time: '10:30',
+    user: '',
+    route: '',
+    completed: ''
+  },
+  {
+    time: '11:00',
+    user: '',
+    route: '',
+    completed: ''
+  },
+  {
+    time: '11:30',
+    user: '',
+    route: '',
+    completed: ''
+  },
+  {
+    time: '12:00',
+    user: '',
+    route: '',
+    completed: ''
+  },
+  {
+    time: '12:30',
+    user: '',
+    route: '',
+    completed: ''
+  },
+  {
+    time: '13:00',
+    user: '',
+    route: '',
+    completed: ''
+  },
+  {
+    time: '13:30',
+    user: '',
+    route: '',
+    completed: ''
+  },
+  {
+    time: '14:00',
+    user: '',
+    route: '',
+    completed: ''
+  },
+  {
+    time: '14:30',
+    user: '',
+    route: '',
+    completed: ''
+  },
+  {
+    time: '15:00',
+    user: '',
+    route: '',
+    completed: ''
+  },
+  {
+    time: '15:30',
+    user: '',
+    route: '',
+    completed: ''
+  },
+  {
+    time: '16:00',
+    user: '',
+    route: '',
+    completed: ''
+  },
+  {
+    time: '16:30',
+    user: '',
+    route: '',
+    completed: ''
+  },
+  {
+    time: '17:00',
+    user: '',
+    route: '',
+    completed: ''
+  },
+  {
+    time: '17:30',
+    user: '',
+    route: '',
+    completed: ''
+  },
+  {
+    time: '18:00',
+    user: '',
+    route: '',
+    completed: ''
+  },
+  {
+    time: '18:30',
+    user: '',
+    route: '',
+    completed: ''
+  },
+  {
+    time: '19:00',
+    user: '',
+    route: '',
+    completed: ''
+  },
+  {
+    time: '20:30',
+    user: '',
+    route: '',
+    completed: ''
+  },
+  {
+    time: '21:00',
+    user: '',
+    route: '',
+    completed: ''
+  },
+  {
+    time: '21:30',
+    user: '',
+    route: '',
+    completed: ''
+  },
+  {
+    time: '22:00',
+    user: '',
+    route: '',
+    completed: ''
+  },
+]
+
 const panes = [
-  { menuItem: 'Monday', render: () => <Tab.Pane><ScheduleTable/></Tab.Pane> },
-  { menuItem: 'Tuesday', render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
-  { menuItem: 'Wednesday', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
-  { menuItem: 'Thursday', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
-  { menuItem: 'Friday', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
-  { menuItem: 'Saturday', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
-  { menuItem: 'Sunday', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
+  { menuItem: 'Monday', render: () => <Tab.Pane><ScheduleTable patrolData={patrolDataExample}/></Tab.Pane> },
+  { menuItem: 'Tuesday', render: () => <Tab.Pane><ScheduleTable patrolData={patrolDataExample}/></Tab.Pane> },
+  { menuItem: 'Wednesday', render: () => <Tab.Pane><ScheduleTable patrolData={patrolDataExample}/></Tab.Pane> },
+  { menuItem: 'Thursday', render: () => <Tab.Pane><ScheduleTable patrolData={patrolDataExample}/></Tab.Pane> },
+  { menuItem: 'Friday', render: () => <Tab.Pane><ScheduleTable patrolData={patrolDataExample}/></Tab.Pane> },
+  { menuItem: 'Saturday', render: () => <Tab.Pane><ScheduleTable patrolData={patrolDataExample}/></Tab.Pane> },
+  { menuItem: 'Sunday', render: () => <Tab.Pane><ScheduleTable patrolData={patrolDataExample}/></Tab.Pane> },
 ]
 
 class Schedule extends React.Component {
