fceec2e72da6963d8437fc7e126340cca734565b
Jorge Aparicio <jorge.aparicio@pomona.edu>
2019-03-30 20:27:58 -0700

    community page improvements

---
+++ src/components/community/MemberCard.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Card, Feed, Image } from 'semantic-ui-react'
+import MemberFeed from './MemberFeed'
+
+
+const eventsInfo = [
+    {
+      name: "Jorge Aparicio",
+      date: "3/30/19",
+      points: "47",
+      url: "Come see the tiger!"
+   
+    },
+    {
+      name: "Jason Cabrera",
+      date: "3/30/19",
+      points: "13",
+      url: "Come see the tiger!2"
+    },
+    {
+      name: "Alex Franklin",
+      date: "3/30/19",
+      points: "47",
+      url: "Come see the tiger!3"
+    },
+    {
+      name: "Anson Lee",
+      date: "3/30/19",
+      points: "123",
+      url: "Come see the tiger!4"
+    },
+   
+  ]
+
+class MemberCard extends React.Component {
+  constructor() {
+    super();
+
+  
+  }
+  render() {
+    const members = eventsInfo.map((text) => <MemberFeed name={text.name} date={text.date} image={text.url} points={text.points}/>)
+        return (
+        <Card>
+        <Card.Content>
+          <Card.Header>Community Members</Card.Header>
+        </Card.Content>
+        <Card.Content>
+        <Feed>
+            {members}
+         </Feed>   
+        </Card.Content>
+      </Card>
+    );
+}
+}
+export default MemberCard
--- src/components/community/MemberList.js
+++ src/components/community/MemberList.js
@@ -3,41 +3,37 @@ import { Button, Popup, Segment} from 'semantic-ui-react'
 import Nav from './../nav/Nav.jsx'
 
 import AddEventForm from './../homepage/AddEventForm'
-import Event from './../homepage/Event'
-
+import MemberFeed from './MemberFeed'
+import MemberCard from './MemberCard'
 
 
 const eventsInfo = [
   {
-    subject: "Tiger sighting",
-    date: "2/3/18",
-    description: "Come see the tiger!"
+    name: "Jorge Aparicio",
+    date: "3/30/19",
+    points: "47",
+    url: "Come see the tiger!"
+ 
   },
   {
-    subject: "Tiger sighting2",
-    date: "2/3/18",
-    description: "Come see the tiger!2"
+    name: "Jason Cabrera",
+    date: "3/30/19",
+    points: "13",
+    url: "Come see the tiger!2"
   },
   {
-    subject: "Tiger sighting3",
-    date: "2/3/18",
-    description: "Come see the tiger!3"
+    name: "Alex Franklin",
+    date: "3/30/19",
+    points: "47",
+    url: "Come see the tiger!3"
   },
   {
-    subject: "Tiger sighting4",
-    date: "2/3/18",
-    description: "Come see the tiger!4"
+    name: "Anson Lee",
+    date: "3/30/19",
+    points: "123",
+    url: "Come see the tiger!4"
   },
-  {
-    subject: "Tiger sighting5",
-    date: "2/3/18",
-    description: "Come see the tiger!5"
-  },
-  {
-    subject: "Tiger sighting6",
-    date: "2/3/18",
-    description: "Come see the tiger!6"
-  }
+ 
 ]
 
 class MemberList extends React.Component {
@@ -45,49 +41,21 @@ class MemberList extends React.Component {
     super();
 
     this.state = {
-      events:[],
+      members:[],
       currEventInfo: {
-        subject: '',
+        name: '',
         date: '',
-        description: ''
+        points: '',
+        url: ''
       }
     };
 
-    this.eventIndex = 0;
+    this.memberIndex = 0;
   }
 
-  updateCurrEvent(event) {
-    const newState = this.event.target.value;
-    this.setState(newState);
-  }
-
-
-  addEvent = () => {
-    if (this.state.currEventInfo === '') {
-      alert('Input something first');
-      return;
-    }
-    const eventInstance = {
-      index: this.eventIndex,
-      content: this.state.currEventInfo
-    };
-    this.eventIndex += 1;
-
-    const prevEvent = this.state.events;
-    const newEvents = [eventInstance, ...prevEvent];
-    const newState = {
-      events: newEvents,
-      currEventInfo: {
-        subject: '',
-        date: '',
-        description: ''
-      }
-    };
-    this.setState(newState);
-  }
 
   render() {
-    const events = eventsInfo.map((text) => <Event subject={text.subject} date={text.date} description={text.description}/>)
+    const members = eventsInfo.map((text) => <MemberFeed name={text.name} date={text.date} image={text.url} points={text.points}/>)
 
     return (
       <div>
@@ -98,7 +66,8 @@ class MemberList extends React.Component {
                 on='click'
               />
               <Segment style={{overflow:'auto', maxHeight:770}} size='massive'>
-                  {events}
+              <MemberCard feed ={members}>
+              </MemberCard>
               </Segment>
       </div>
     );
