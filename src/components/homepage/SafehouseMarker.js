42129405cd78ed0c12d2c62210387132a183c0a2
alexmfranklin <amfa2017@mymail.pomona.edu>
2019-03-30 17:10:38 -0700

    adding safehouse markers to the google map

---
+++ src/components/homepage/SafehouseMarker.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Icon } from 'semantic-ui-react'
+
+class SafehouseMarker extends React.Component {
+  render() {
+    return (
+      <div>
+        <Icon name='home' size='big' color='green'/>
+      </div>
+    )
+  }
+}
+
+export default SafehouseMarker
