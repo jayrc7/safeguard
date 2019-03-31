a27ee5c76a22d091e8f5ec248c0fe6c9367f49b4
alexmfranklin <amfa2017@mymail.pomona.edu>
2019-03-30 20:44:02 -0700

    adding titles to community page

--- src/components/community/Community.js
+++ src/components/community/Community.js
@@ -1,30 +1,31 @@
 import React from 'react';
-import { Grid, Image} from 'semantic-ui-react'
+import { Grid, Image, Header } from 'semantic-ui-react'
 import Nav from './../nav/Nav.jsx'
 import MemberList from './MemberList'
 import ChatImg from './chat.jpg'
 import CommunityEvents from './CommunityEvents'
 class Community extends React.Component{
     render() {
-        
-    
+
+
         return (
           <div>
             <Nav/>
             <Grid centered='true' padded='true'>
               <Grid.Row columns={3}>
                 <Grid.Column width="4">
-                <label> Community Members</label>
+                <Header as='h3' dividing size='huge'>Community Members</Header>
                 <MemberList/>
-              
+
                 </Grid.Column>
                 <Grid.Column width="7">
                 <img src={ChatImg}/>
-              
+
                 </Grid.Column>
                 <Grid.Column width="4">
+                <Header as='h3' dividing size='huge'>Notice Board</Header>
                 <CommunityEvents/>
-              
+
                 </Grid.Column>
               </Grid.Row>
             </Grid>
@@ -32,5 +33,5 @@ class Community extends React.Component{
         );
       }
     }
-    
+
 export default Community
